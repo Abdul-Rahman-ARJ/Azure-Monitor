0c27481ae8280bf61e58037f9e7abf9dcbb6a50b
Abdul Rahman <id16abdulrahman@gmail.com>
2023-05-02 19:53:51 +0530

    pandi effect

--- GetCPUWithParams/index.js
+++ GetCPUWithParams/index.js
@@ -1,99 +1,109 @@
 // importing sql module
 const sql = require('mssql');
 
-async function getSqlCpuUsage(server, DB) {
-    const config = {
-        server: server,
-        database: DB,
-        user: 'atrainuser',
-        password: 'rt53#$%@fgt5$3',
-        options: {
-            encrypt: true // if using Azure
-        }
-    };
-    try {
-        await sql.connect(config);
-        const result = await sql.query(`
-        DECLARE @dbname SYSNAME = NULL 
-        SELECT
-                sdes.session_id        ,sdes.login_time        ,sdes.last_request_start_time       ,sdes.last_request_end_time       ,sdes.is_user_process       ,sdes.host_name       ,sdes.program_name       ,sdes.login_name       ,sdes.status
-               ,sdec.num_reads       ,sdec.num_writes       ,sdec.last_read       ,sdec.last_write       ,sdes.reads       ,sdes.logical_reads       ,sdes.writes              ,DatabaseName = COALESCE( db_name(sdes.database_id),  N'')
-               ,sdest.ObjName    ,sdes.client_interface_name    ,sdes.nt_domain    ,sdes.nt_user_name    ,sdec.client_net_address    ,sdec.local_net_address    ,sdest.Query    ,KillCommand  = 'Kill '+ CAST(sdes.session_id  AS VARCHAR)
-        from sys.dm_tran_locks t INNER JOIN sys.dm_exec_sessions sdes
-                ON t.request_session_id = sdes.session_id
-        LEFT OUTER JOIN sys.dm_exec_connections AS sdec
-                ON sdec.session_id = sdes.session_id
-        OUTER APPLY (
-        
-                        SELECT DB_NAME(dbid) AS DatabaseName
-                            ,OBJECT_NAME(objectid) AS ObjName
-        ,COALESCE((
-        SELECT TEXT AS [processing-instruction(definition)]
-        FROM sys.dm_exec_sql_text(sdec.most_recent_sql_handle)
-        FOR XML PATH('')
-        ,TYPE
-        ), '') AS Query
-        
-        FROM sys.dm_exec_sql_text(sdec.most_recent_sql_handle)
-        
-        ) sdest
-        where t.resource_type = 'database'
-        and t.resource_database_id = CASE WHEN @dbname IS NULL
-        THEN t.resource_database_id
-        ELSE DB_ID(@dbname)
-        END
-        and t.request_type = 'LOCK'
-        and t.request_status = 'GRANT'
-        `);
-        sql.close();
-        return result
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-
-
 module.exports = async function (context, req) {
     let { method } = req;
-    ADF = false
-    QV = false
-    PBI = false
+    ADFisRunning = false
+    QVisRunning = false
+    PBIisRunnning = false
+    const { server, DB, Puser } = req.body
+    console.log(Puser)
     if (method == 'POST') {
-        const { server, DB } = req.body
-        res = await getSqlCpuUsage(server, DB);
-        // console.log(res.recordset)
+        res = await getSqlCpuUsage(server, DB,Puser );
+        console.log(res)
         let data = res.recordset;
         result = []
+        context.log(`There are ${data.length} fetched. ${DB}`)
         data.map((item) => {
-            // check ADF is runnig
-            if (item.host_name == "dw_admin") {
+            // check ADFisRunning is runnig
+            if (item.login_name == "dw_admin") {
                 if (item.status == "running") {
-                    result.push({ item, ADF:true })
+                    ADFisRunning = true;
+                    result.push({ item, ADFisRunning:ADFisRunning })
                 }
-                console.log("ADF running")
+                console.log("ADFisRunning running")
 
             }
-            // check QV is runnig
+            // check QVisRunning is runnig
             else if (item.host_name.startsWith("QV")) {
                 // console.log("pd1mdwk000Q3P")
                 if (item.status == "running") {
-                    result.push({ item, QV:true })
+                    QVisRunning=true;
+                    result.push({ item, QVisRunning:QVisRunning })
                 }
             }
-            // check PBI is runnig
+            // check PBIisRunnning is runnig
             // "program_name": "Mashup Engine (PowerBIPremium-Import)",
             else if (item.program_name == "Mashup Engine (PowerBIPremium-Import)") {
                 if (item.status == "running") {
-                    result.push({ item, PBI:true })
+                    PBIisRunnning = true;
+                    result.push({ item, PBIisRunnning:PBIisRunnning })
                 }
             }
         })
     }
-
+    context.log('All processed', DB);
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: { result }
+        body: { result , ADFisRunning ,QVisRunning , PBIisRunnning , res}
         // body: { res }
     };
+    context.done();
+
+    async function getSqlCpuUsage(server, DB, Puser) {
+        const config = {
+            server: server,
+            database: DB,
+            user: Puser?"aprduser":'atrainuser',
+            password: 'rt53#$%@fgt5$3',
+            options: {
+                encrypt: true // if using Azure
+            }
+        };
+        try {
+            await sql.connect(config);
+            const result = await sql.query(`
+            DECLARE @dbname SYSNAME = NULL 
+            SELECT
+                    sdes.session_id        ,sdes.login_time        ,sdes.last_request_start_time       ,sdes.last_request_end_time       ,sdes.is_user_process       ,sdes.host_name       ,sdes.program_name       ,sdes.login_name       ,sdes.status
+                   ,sdec.num_reads       ,sdec.num_writes       ,sdec.last_read       ,sdec.last_write       ,sdes.reads       ,sdes.logical_reads       ,sdes.writes              ,DatabaseName = COALESCE( db_name(sdes.database_id),  N'')
+                   ,sdest.ObjName    ,sdes.client_interface_name    ,sdes.nt_domain    ,sdes.nt_user_name    ,sdec.client_net_address    ,sdec.local_net_address    ,sdest.Query    ,KillCommand  = 'Kill '+ CAST(sdes.session_id  AS VARCHAR)
+            from sys.dm_tran_locks t INNER JOIN sys.dm_exec_sessions sdes
+                    ON t.request_session_id = sdes.session_id
+            LEFT OUTER JOIN sys.dm_exec_connections AS sdec
+                    ON sdec.session_id = sdes.session_id
+            OUTER APPLY (
+            
+                            SELECT DB_NAME(dbid) AS DatabaseName
+                                ,OBJECT_NAME(objectid) AS ObjName
+            ,COALESCE((
+            SELECT TEXT AS [processing-instruction(definition)]
+            FROM sys.dm_exec_sql_text(sdec.most_recent_sql_handle)
+            FOR XML PATH('')
+            ,TYPE
+            ), '') AS Query
+            
+            FROM sys.dm_exec_sql_text(sdec.most_recent_sql_handle)
+            
+            ) sdest
+            where t.resource_type = 'database'
+            and t.resource_database_id = CASE WHEN @dbname IS NULL
+            THEN t.resource_database_id
+            ELSE DB_ID(@dbname)
+            END
+            and t.request_type = 'LOCK'
+            and t.request_status = 'GRANT'
+            `);
+            sql.close();
+            return result
+        } catch (err) {
+            context.log(`Error while executing the sql: ${err}, ${DB}`);
+            try{
+                sql.close();
+            }catch(err2)
+            {
+                context.log(`Error at closing the connection: ${err2}, ${DB}`);
+            }
+        }
+    }
 }
