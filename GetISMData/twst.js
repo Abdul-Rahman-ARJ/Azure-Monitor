17a2f156d9284529891ce1e6f18f3882c18a872e
Abdul Rahman <id16abdulrahman@gmail.com>
2023-12-23 00:58:46 +0530

    havalli

---
+++ GetISMData/twst.js
@@ -0,0 +1,19 @@
+setProperty(addProperty(addProperty(addProperty(addProperty(addProperty(addProperty(items('AtlasFiveReports_Loop'),'id',body('Parse_JSON')?['id']),'tenantname','Todd'),'asofDateForAtlasfive',body('Parse_JSON')?['asofDateForAtlasfive']),'entityId',body('Parse_JSON')?['entity']),'relationship',body('Parse_JSON')?['relationship']),'portfolio',body('Parse_JSON')?['portfolio']),'reportseq',actionBody('AtlasFiveFilterArray')[0]['reportseq'])
+
+setProperty(addProperty(addProperty(addProperty(addProperty(items('AtlasFiveReports_Loop'),'id',body('Parse_JSON')?['id']),'tenantname','Todd-Training'),'asofDateForAtlasfive',body('Parse_JSON')?['asofDateForAtlasfive']),'entityId',body('Parse_JSON')?['entity']),'reportseq',actionBody('AtlasFiveFilterArray')[0]['reportseq'])
+
+setProperty(addProperty(addProperty(addProperty(addProperty(items('AtlasFiveReports_Loop'),'id',body('Parse_JSON')?['id']),'tenantname','Todd'),'asofDateForAtlasfive',body('Parse_JSON')?['asofDateForAtlasfive']),'entityId',body('Parse_JSON')?['entity']),'reportseq',actionBody('AtlasFiveFilterArray')[0]['reportseq'])
+\
+
+
+{"server": "elastic-sqlserver005.database.windows.net", "authentication": { "type": "default", "options": { "userName": "aprduser", "password": "rt53#$%@fgt5$3" } }, "options": { "encrypt": true, "database": "landlord", "trustServerCertificate": true }}
+\
+{"server": "elastic-sqlserver005.database.windows.net", "authentication": { "type": "default", "options": { "userName": "aprduser", "password": "rt53#$%@fgt5$3" } }, "options": { "encrypt": true, "database": "landlord", "trustServerCertificate": true }}
+
+setProperty(addProperty(addProperty(items('PBI_loop'),'id',body('Parse_JSON')?['id']),'tenantname','ToddUat'),'reportseq',actionBody('FilterArrayPBI')[0]['reportseq'])
+
+{"url":"https://login.microsoftonline.com/e73cef03-a4f0-4299-adf4-a6b3a42eb486/oauth2/token","body":"client_id=0456c694-4c4f-471c-b602-975add02a0af&client_secret=eCQ8Q~h8Wv7ENeIsZcgklbde5ktbwReydLLkSaF.&grant_type=client_credentials&resource=https://analysis.windows.net/powerbi/api"}
+
+{"url":"https://login.microsoftonline.com/e73cef03-a4f0-4299-adf4-a6b3a42eb486/oauth2/token","body":"client_id=0456c694-4c4f-471c-b602-975add02a0af&client_secret=eCQ8Q~h8Wv7ENeIsZcgklbde5ktbwReydLLkSaF&grant_type=client_credentials&resource=https://analysis.windows.net/powerbi/api"}
+
+{"url":"https://login.microsoftonline.com/e73cef03-a4f0-4299-adf4-a6b3a42eb486/oauth2/token","body":"client_id=b53c820f-1920-40ba-aea3-2763b14e1fd5&client_secret=ZM18Q~IlvJcGxzH85KqPZmplcA2RlLshNBH9NcHN&grant_type=client_credentials&resource=https://analysis.windows.net/powerbi/api"}
